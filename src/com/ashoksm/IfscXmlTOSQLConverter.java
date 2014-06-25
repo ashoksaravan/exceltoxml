de60784d0ecf2f2f1b5ba2676420d7001cba9555
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2014-06-25 23:03:30 +0530

    STD

--- src/com/ashoksm/IfscXmlTOSQLConverter.java
+++ src/com/ashoksm/IfscXmlTOSQLConverter.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -13,6 +15,7 @@ public class IfscXmlTOSQLConverter {
 	private static final String SQL = "INSERT INTO bank_branch_t VALUES('<BranchName>', '<City>', '<Address>', '<Contact>', '<MicrCode>', '<IfscCode>', <BankLoc>);";
 
 	public static void main(String[] args) throws Exception {
+		Set<String> locSet = new HashSet<String>();
 		XStream xStream = new XStream();
 		xStream.alias("bankbranch", BankBranch.class);
 		xStream.alias("bankBranchs", BankBranchs.class);
@@ -27,22 +30,31 @@ public class IfscXmlTOSQLConverter {
 			for (BankBranch bankBranch : bankBranchs.getBankBranchs()) {
 				String line = null;
 				try {
-					line = SQL.replaceAll("<BranchName>", bankBranch.getName().trim().replaceAll("'", "''"))
-							.replaceAll("<City>", bankBranch.getCity().trim().replaceAll("'", "''"))
-							.replaceAll("<Address>", bankBranch.getAddress().trim().replaceAll("'", "''"))
-							.replaceAll("<Contact>", bankBranch.getContact().trim().replaceAll("'", "''"))
-							.replaceAll("<MicrCode>", bankBranch.getMicrCode())
-							.replaceAll("<IfscCode>", bankBranch.getIfscCode())
-							.replaceAll("<BankLoc>", bankBranch.getBankLoc().toString());
+					String loc = bankBranch.getName().trim() + "_" + bankBranch.getIfscCode() + "_"
+							+ bankBranch.getBankLoc().toString();
+					if (locSet.add(loc)) {
+						line = SQL.replaceAll("<BranchName>", bankBranch.getName().trim().replaceAll("'", "''"))
+								.replaceAll("<City>", bankBranch.getCity().trim().replaceAll("'", "''"))
+								.replaceAll("<Address>", bankBranch.getAddress().trim().replaceAll("'", "''"))
+								.replaceAll("<Contact>", bankBranch.getContact().trim().replaceAll("'", "''"))
+								.replaceAll("<MicrCode>", bankBranch.getMicrCode())
+								.replaceAll("<IfscCode>", bankBranch.getIfscCode())
+								.replaceAll("<BankLoc>", bankBranch.getBankLoc().toString());
+					} else {
+						System.out.println(loc);
+					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
 					System.out.println("In exception ::::: " + bankBranch.getName());
 				}
-				writer.write(line);
-				writer.newLine();
+				if (line != null) {
+					writer.write(line);
+					writer.newLine();
+				}
 			}
 			writer.flush();
 			writer.close();
 		}
+		System.out.println(locSet.size());
 	}
 }
