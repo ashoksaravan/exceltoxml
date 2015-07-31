1962919b631ab563bba0acf0357283a7e89d8b5f
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2015-07-31 08:26:52 +0530

    Process pincode form csv

--- src/com/ashoksm/StateAndDistrictSQLGenerator.java
+++ src/com/ashoksm/StateAndDistrictSQLGenerator.java
@@ -16,12 +16,12 @@ import org.apache.poi.ss.usermodel.Row;
 public class StateAndDistrictSQLGenerator {
 
 	private static final String STATE_SQL = "INSERT INTO state_t VALUES(<StateCode>, '<StateName>');";
-	
+
 	private static final String LOCATION_SQL = "INSERT INTO location_t VALUES(<LocationCode>, '<LocationName>');";
 
 	private static final String DISTRICT_SQL = "INSERT INTO district_t VALUES(<StateCode>, <DistrictCode>, '<DistrictName>');";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		createStateSQL();
 		createLocationsSQL();
 		createDistrictSQL();
@@ -34,7 +34,7 @@ public class StateAndDistrictSQLGenerator {
 	private static void createStateSQL() throws IOException {
 		File stateExcel = new File("E:\\Ashok\\Dropbox\\MyDetails\\pinfinder\\states.xls");
 		BufferedWriter writer = new BufferedWriter(new FileWriter(
-				"E:\\Android\\workspace\\OfflinePinFinder\\assets\\sql\\pincode\\states.sql"));
+				"E:\\Android\\workspace\\OfflinePinFinder\\app\\src\\main\\assets\\sql\\pincode\\states.sql"));
 
 		FileInputStream file = new FileInputStream(stateExcel);
 
@@ -63,13 +63,13 @@ public class StateAndDistrictSQLGenerator {
 	}
 
 	/**
-	 * @throws IOException
+	 * @throws Exception
 	 * @throws FileNotFoundException
 	 */
-	private static void createLocationsSQL() throws IOException {
-		File stateExcel = new File("E:\\Ashok\\Dropbox\\MyDetails\\pinfinder\\locations.xls");
+	private static void createLocationsSQL() throws Exception {
+		File stateExcel = new File("E:\\Ashok\\Dropbox\\MyDetails\\pinfinder\\taluk.xls");
 		BufferedWriter writer = new BufferedWriter(new FileWriter(
-				"E:\\Android\\workspace\\OfflinePinFinder\\assets\\sql\\pincode\\locations.sql"));
+				"E:\\Android\\workspace\\OfflinePinFinder\\app\\src\\main\\assets\\sql\\pincode\\locations.sql"));
 
 		FileInputStream file = new FileInputStream(stateExcel);
 
@@ -89,7 +89,14 @@ public class StateAndDistrictSQLGenerator {
 			line = LOCATION_SQL.replaceAll("<LocationCode>",
 					String.valueOf(new Double(cell.getNumericCellValue()).intValue()));
 			Cell cell1 = row.getCell(1);
-			line = line.replaceAll("<LocationName>", cell1.getStringCellValue().trim().replaceAll("'", "''"));
+			try {
+				line = line.replaceAll("<LocationName>", cell1.getStringCellValue().trim().replaceAll("'", "''"));
+			} catch (Exception ex) {
+				System.out.println(cell1.getStringCellValue());
+				writer.flush();
+				writer.close();
+				throw new Exception(cell1.getStringCellValue(), ex);
+			}
 			writer.write(line);
 			writer.newLine();
 		}
@@ -102,36 +109,35 @@ public class StateAndDistrictSQLGenerator {
 	 */
 	private static void createDistrictSQL() throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(
-				"E:\\Android\\workspace\\OfflinePinFinder\\assets\\sql\\pincode\\district.sql"));
-		File root = new File("E:\\Ashok\\Dropbox\\MyDetails\\pinfinder\\Districts");
-		for (final File district : root.listFiles()) {
+				"E:\\Android\\workspace\\OfflinePinFinder\\app\\src\\main\\assets\\sql\\pincode\\district.sql"));
+		File root = new File("E:\\Ashok\\Dropbox\\MyDetails\\pinfinder\\district.xls");
 
-			FileInputStream file = new FileInputStream(district);
+		FileInputStream file = new FileInputStream(root);
 
-			// Get the workbook instance for XLS file
-			HSSFWorkbook workbook = new HSSFWorkbook(file);
+		// Get the workbook instance for XLS file
+		HSSFWorkbook workbook = new HSSFWorkbook(file);
 
-			// Get first sheet from the workbook
-			HSSFSheet sheet = workbook.getSheetAt(0);
+		// Get first sheet from the workbook
+		HSSFSheet sheet = workbook.getSheetAt(0);
 
-			// Iterate through each rows from first sheet
-			Iterator<Row> rowIterator = sheet.iterator();
-			while (rowIterator.hasNext()) {
-				String line = null;
-				Row row = rowIterator.next();
-				// For each row, iterate through each columns
-				Cell cell = row.getCell(0);
-				line = DISTRICT_SQL.replaceAll("<StateCode>",
-						String.valueOf(new Double(cell.getNumericCellValue()).intValue()));
-				Cell cell1 = row.getCell(1);
-				line = line.replaceAll("<DistrictCode>", String.valueOf(new Double(cell1.getNumericCellValue()).intValue()));
-				Cell cell2 = row.getCell(2);
-				line = line.replaceAll("<DistrictName>",cell2.getStringCellValue());
-				writer.write(line);
-				writer.newLine();
-			}
-
+		// Iterate through each rows from first sheet
+		Iterator<Row> rowIterator = sheet.iterator();
+		while (rowIterator.hasNext()) {
+			String line = null;
+			Row row = rowIterator.next();
+			// For each row, iterate through each columns
+			Cell cell = row.getCell(0);
+			line = DISTRICT_SQL.replaceAll("<StateCode>",
+					String.valueOf(new Double(cell.getNumericCellValue()).intValue()));
+			Cell cell1 = row.getCell(1);
+			line = line
+					.replaceAll("<DistrictCode>", String.valueOf(new Double(cell1.getNumericCellValue()).intValue()));
+			Cell cell2 = row.getCell(2);
+			line = line.replaceAll("<DistrictName>", cell2.getStringCellValue());
+			writer.write(line);
+			writer.newLine();
 		}
+
 		writer.flush();
 		writer.close();
 	}
