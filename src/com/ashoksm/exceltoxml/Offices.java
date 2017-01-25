1edd10c21ef1b4a73a960d548066d892829dcf57
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2017-01-25 23:20:36 +0530

    Changed package name.

---
+++ src/com/ashoksm/exceltoxml/Offices.java
@@ -0,0 +1,25 @@
+package com.ashoksm.exceltoxml;
+
+import java.util.List;
+
+public class Offices {
+
+	public Offices(){
+	}
+
+	private List<Office> offices;
+
+	/**
+	 * @return the offices
+	 */
+	public List<Office> getOffices() {
+		return offices;
+	}
+
+	/**
+	 * @param officesIn the offices to set
+	 */
+	public void setOffices(List<Office> officesIn) {
+		this.offices = officesIn;
+	}
+} 
