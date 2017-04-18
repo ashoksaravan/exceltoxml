7f459f6e58ad2a82cffd536829e3b3be30c14943
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2017-04-18 08:27:29 +0530

    Initial Commit

---
+++ src/main/java/com/ashoksm/exceltoxml/CorrectDistrictNames.java
@@ -0,0 +1,56 @@
+package com.ashoksm.exceltoxml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+
+public class CorrectDistrictNames {
+
+	public static void main(String[] args) throws IOException {
+		File file = new File("E:/Ashok/ifsc/IFCB2009_10.xls");
+		FileInputStream inputStream = new FileInputStream(file);
+
+		// Get the workbook instance for XLS file
+		HSSFWorkbook workBook = new HSSFWorkbook(inputStream);
+
+		// Get first sheet from the workbook
+		HSSFSheet bankSheet = workBook.getSheetAt(0);
+		// Iterate through each rows from first sheet
+		Iterator<Row> rowIt = bankSheet.iterator();
+		boolean flag = true;
+		while (rowIt.hasNext()) {
+			Row row = rowIt.next();
+			if (flag) {
+				flag = false;
+				continue;
+			}
+			String district = row.getCell(7).getStringCellValue();
+			String newDistrict = null;
+			if (district.contains("DIST.")) {
+				newDistrict = district.substring(district.indexOf("DIST.") + 5);
+			} else if (district.contains("DIST")) {
+				newDistrict = district.substring(district.indexOf("DIST") + 4);
+			} else if (district.contains("DISTT")) {
+				newDistrict = district.substring(district.indexOf("DISTT") + 5);
+			}
+			if (newDistrict != null) {
+				newDistrict = newDistrict.trim();
+				if (newDistrict.contains(")") && !newDistrict.contains("(")) {
+					newDistrict = newDistrict.replace(')', ' ').trim();
+				}
+				row.getCell(7).setCellValue(newDistrict);
+			}
+		}
+		FileOutputStream out = new FileOutputStream(file);
+		workBook.write(out);
+		out.close();
+		workBook.close();
+	}
+
+}
